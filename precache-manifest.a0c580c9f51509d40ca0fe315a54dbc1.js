30a7db287befb4336ae7266dc84debafcc890d29
Andee Tao <andeetao@Andees-iMac.attlocal.net>
2018-12-16 23:15:13 -0800

    Updates

---
+++ precache-manifest.a0c580c9f51509d40ca0fe315a54dbc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eb6ec3f72b085ca409e",
+    "url": "/tomatotomato/static/css/main.54bf99d2.chunk.css"
+  },
+  {
+    "revision": "5eb6ec3f72b085ca409e",
+    "url": "/tomatotomato/static/js/main.5eb6ec3f.chunk.js"
+  },
+  {
+    "revision": "f7ebf944001c77dad8a8",
+    "url": "/tomatotomato/static/js/1.f7ebf944.chunk.js"
+  },
+  {
+    "revision": "986aaa06d71f9583b119",
+    "url": "/tomatotomato/static/js/runtime~main.986aaa06.js"
+  },
+  {
+    "revision": "23bd14664699fb80613b01cba84ed2cd",
+    "url": "/tomatotomato/index.html"
+  }
+];
