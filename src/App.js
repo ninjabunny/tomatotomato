2d0cd5d4e9718cace0d2e60cc222c942efb5f2c7
Andee Tao <andeetao@Andees-iMac.attlocal.net>
2018-12-16 23:14:48 -0800

    carded

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
 import AddIcon from '@material-ui/icons/Add';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const LONGEST_WORD_LENGTH = 6
 
@@ -21,7 +24,7 @@ const cards = [{
 }, ]
 const addCard = () => cards[Math.floor(Math.random() * 4)]
 
-const SpanSpace = () => < span > &nbsp; < /span>
+const SpanSpace = () => < span > & nbsp; < /span>
 
 class App extends Component {
   defaultState = () => [cards[0], ...Array(3).fill(1).map(() => addCard())]
@@ -38,65 +41,74 @@ class App extends Component {
     });
   };
   render() {
-      const {
-        value,
-        tablue
-      } = this.state;
-      return ( < div >
-        <
-        div className = "tablue" > {
-          tablue.map(({
-              name
-            }, index) => < span key = {
-              `${name}-${index}`
-            } > {
-              name
-            } < SpanSpace / > < /span>)}</div >
+    const {
+      value,
+      tablue
+    } = this.state;
+    return ( < div >
+      <
+      div className = "tablue" >
+      <Grid container spacing={8}>
 
-            <
-            BottomNavigation className = "navbar"
-            value = {
-              value
-            }
-            onChange = {
-              this.handleChange
-            }
-            showLabels >
-            <
-            BottomNavigationAction label = "Reset"
-            icon = { < RefreshIcon / >
+       {
+        tablue.map(({
+            name
+          }, index) =>
+          <Grid item xs={3}>
+          <
+          Card className = 'card'
+          key = {
+            `${name}-${index}`
+          } > < Typography > {
+            name
+          } < /Typography></Card ></Grid>
+        )
+      } </Grid>< /div >
 
-            }
-            onClick = {
-              () => this.setState({
-                tablue: this.defaultState()
-              })
-            }
-            /> <
-            BottomNavigationAction label = "Condense"
-            icon = { < CompareArrowsIcon / >
-            }
-            onClick = {
-              () =>
-              this.setState({
-                tablue: tablue.filter(({
-                  name
-                }, index, arr) => index ? name !== arr[index - 1].name : true)
-              })
-            }
-            /> <
-            BottomNavigationAction label = "Add"
-            icon = { < AddIcon / >
-            }
-            onClick = {
-              () => this.setState({
-                tablue: [...tablue, addCard()]
-              })
-            }
-            /> < /
-            BottomNavigation > < /div>
-          );
-        }
+      <
+      BottomNavigation className = "navbar"
+      value = {
+        value
       }
+      onChange = {
+        this.handleChange
+      }
+      showLabels >
+      <
+      BottomNavigationAction label = "Reset"
+      icon = { < RefreshIcon / >
 
-      export default App;
+      }
+      onClick = {
+        () => this.setState({
+          tablue: this.defaultState()
+        })
+      }
+      /> <
+      BottomNavigationAction label = "Condense"
+      icon = { < CompareArrowsIcon / >
+      }
+      onClick = {
+        () =>
+        this.setState({
+          tablue: tablue.filter(({
+            name
+          }, index, arr) => index ? name !== arr[index - 1].name : true)
+        })
+      }
+      /> <
+      BottomNavigationAction label = "Add"
+      icon = { < AddIcon / >
+      }
+      onClick = {
+        () => this.setState({
+          tablue: [...tablue, addCard()]
+        })
+      }
+      /> < /
+      BottomNavigation > < /div>
+    );
+  }
+}
+
+export default App;
